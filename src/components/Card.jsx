99e2caaf16c4d74a5c46bfab6db25e7aa4947137
wakar-malik <wakarmalik9@gmail.com>
2023-07-13 02:59:56 +0530

    concept of composition

---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Card.css";
+
+function Card(props) {
+  const classes = "card " + props.className;
+
+  return <section className={classes}>{props.children}</section>;
+}
+
+export default Card;
