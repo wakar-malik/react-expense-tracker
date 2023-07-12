7f5ed85d9f4bed662c7bfd64a083b2e3c4e940ad
wakar-malik <wakarmalik9@gmail.com>
2023-07-13 01:20:32 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <h2>Lets get started</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
