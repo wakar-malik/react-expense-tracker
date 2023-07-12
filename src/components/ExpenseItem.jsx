e36b4f3940bdb98b8c6ebeaa07b6f03a19c78823
wakar-malik <wakarmalik9@gmail.com>
2023-07-13 01:27:01 +0530

    added css style

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function ExpenseItem() {
   return (
-    <div>
+    <div className="expense-item">
       <div>March</div>
-      <div>
+      <div className="expense-item__description">
         <h2>Car Insurance</h2>
-        <div>$294.67</div>
+        <div className="expense-item__price">$294.67</div>
       </div>
     </div>
   );
