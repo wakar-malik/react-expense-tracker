84ae12b54aeb7eabbd5c19bcaa7d175991d22c98
wakar-malik <wakarmalik9@gmail.com>
2023-07-14 00:40:57 +0530

    first summary, closer look at jsx, organizing component files, alternative function syntax

--- src/components/Expenses/ExpenseDate.jsx
+++ src/components/Expenses/ExpenseDate.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./ExpenseDate.css";
 
-function ExpenseDate(props) {
+const ExpenseDate = (props) => {
   const month = props.date.toLocaleString("en-US", { month: "long" });
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
   const year = props.date.getFullYear();
+
+  
 
   return (
     <div className="expense-date">
@@ -13,6 +15,6 @@ function ExpenseDate(props) {
       <div className="expense-date__day">{day}</div>
     </div>
   );
-}
+};
 
 export default ExpenseDate;
