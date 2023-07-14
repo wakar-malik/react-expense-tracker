fb8eb9f50624a78d8d61067e829a851f2d915f02
wakar-malik <wakarmalik9@gmail.com>
2023-07-14 10:53:15 +0530

    child to parent communication, assignment 2

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,35 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
+import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filteredChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredList = props.items.filter((el) => {
+    return el.date.getFullYear() === +filteredYear;
+  });
+
+  let listExpense;
+
+  if (filteredList.length > 0) {
+    listExpense = filteredList.map((el) => (
+      <ExpenseItem
+        title={el.title}
+        amount={el.amount}
+        date={el.date}
+        key={el.id}
+      />
+    ));
+  } else {
+    listExpense = (
+      <p style={{ color: "#fff", textAlign: "center" }}>No Data Found!</p>
+    );
+  }
+
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filteredChangeHandler}
+        />
+        {listExpense}
+      </Card>
+    </div>
   );
 };
 
--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -2,10 +2,19 @@ import React from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
-function NewExpense() {
+function NewExpense(props) {
+  const onSaveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseDate = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+
+    props.onAddExpense(expenseDate);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} />
     </div>
   );
 }
